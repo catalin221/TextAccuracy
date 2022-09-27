2dd096c08c33c8f170ff2b017afb367365dacf0d
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2022-09-27 16:42:02 +0300

    Add project files.

---
+++ TextAccuracy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TextAccuracy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
